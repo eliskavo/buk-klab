6b3c64d6d556fca05864e29722da951c79c5bf66
Thi Phuong Vo | Eliška <89915964+eliskavo@users.noreply.github.com>
2025-01-21 21:41:28 +0100

    Add footer content (#22)
    * Create footer content
    
    * Add scroll-to-top functionality on route change
    
    * Add fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,75 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import waving_girl from '../../assets/images/waving_girl.png';
 import style from './Footer.module.scss';
 
 export const Footer: React.FC = () => (
   <footer className={style.footer}>
-    <div>Footer</div>
+    <div className={style.mainContent}>
+      <h1 className={style.logo}>buk klab</h1>
+
+      <section>
+        <h3 className={style.sectionTitle}>navigation</h3>
+        <nav aria-label="Footer navigation">
+          <ul className={style.linkList}>
+            <li>
+              <Link to="/books" className={style.link}>
+                books
+              </Link>
+            </li>
+            <li>
+              <Link to="/joinclub" className={style.link}>
+                join club
+              </Link>
+            </li>
+            <li>
+              <Link to="/members" className={style.link}>
+                members
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className={style.link}>
+                about
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
+
+      <section>
+        <h3 className={style.sectionTitle}>app</h3>
+        <nav>
+          <ul className={style.linkList}>
+            <li>
+              <Link to="/signin" className={style.link}>
+                sign in
+              </Link>
+            </li>
+            <li>
+              <Link to="/register" className={style.link}>
+                sign up
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </div>
+
+    <div className={style.bottomBar}>
+      <p>
+        created by{' '}
+        <a
+          className={style.profileLink}
+          href="https://github.com/eliskavo"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Eliška Võ
+        </a>
+      </p>
+    </div>
+
+    <img src={waving_girl} alt="" className={style.image} />
   </footer>
 );
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,25 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import { Navbar } from '../Navbar/Navbar';
 import { Footer } from '../Footer/Footer';
 import { ChildrenFC } from '../../utils/type';
 import style from './Layout.module.scss';
 
-export const Layout: ChildrenFC = ({ children }) => (
-  <div className={style.pageContainer}>
-    <Navbar />
-    <div className={style.content}>{children}</div>
-    <Footer />
-  </div>
-);
+export const Layout: ChildrenFC = ({ children }) => {
+  const location = useLocation;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return (
+    <div className={style.pageContainer}>
+      <div className={style.content}>
+        <Navbar />
+        <main className={style.mainContent}>{children}</main>
+      </div>
+      <Footer />
+    </div>
+  );
+};
